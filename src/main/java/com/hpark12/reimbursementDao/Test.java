8ddd2348f6314f1a7156b9059b7a1ab820904981
hpark12 <hyunjae.v4@gmail.com>
2018-05-11 09:32:55 -0400

    ojdbc7

--- src/main/java/com/hpark12/reimbursementDao/Test.java
+++ src/main/java/com/hpark12/reimbursementDao/Test.java
@@ -1,22 +1,32 @@
 package com.hpark12.reimbursementDao;
 
-import com.hpark12.reimbursementModel.User;
-import com.hpark12.reimbursementService.ReimbursementService;
-
 public class Test {
 
     public static void main(String[] args) {
 
-        User user = ReimbursementService.loginUser("Batman@gmail.com", "Gotham");
-
-        //System.out.println(ReimbursementService.addReimbursement(user, 100000, "Batarangs"));
-
-        System.out.println(ReimbursementService.viewPending(user));
-
-        String test = ReimbursementService.viewAllPending(user).toString();
-
-        System.out.println(test);
+//        User user = ReimbursementService.loginUser("Batman@gmail.com", "Gotham");
+//
+//        //System.out.println(ReimbursementService.addReimbursement(user, 100000, "Batarangs"));
+//
+//        System.out.println(ReimbursementService.viewPending(user));
+//
+//        String test = ReimbursementService.viewAllPending(user).toString();
+//
+//        System.out.println(test);
+        int num = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(num);
+                if (num == 0) {
+                    num = 1;
+                } else {
+                    num = 0;
+                }
+            }
+            System.out.println();
+        }
 
 
     }
+
 }
